77c39f41e490726fd4b725a0c4c8d7cadab33f83
Masharipov <56969653+Masharipov@users.noreply.github.com>
2024-08-09 21:08:22 +0300

    Update

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Hemodynamic fMRI BOLD deconvolution
 
 
-This repository provides a Python function to perform hemodynamic deconvolution on fMRI BOLD signals. The deconvolution process is based on a discrete cosine set and ridge regression, converting preprocessed BOLD signals into neuronal time series.
+This repository provides a Python function to perform hemodynamic deconvolution of fMRI BOLD signals. The deconvolution process is based on a discrete cosine set and ridge regression, converting preprocessed BOLD signals into neuronal time series.
 
-Function: ridge_regress_deconvolution
-The ridge_regress_deconvolution function deconvolves a preprocessed BOLD signal into neuronal time series without using confound regressors (e.g., motion) and without performing whitening and temporal filtering. The input BOLD signal must already be preprocessed.
+Function: **ridge_regress_deconvolution**
+The **ridge_regress_deconvolution** function deconvolves a preprocessed BOLD signal into neuronal time series without using confound regressors (e.g., motion) and without performing whitening and temporal filtering. The input BOLD signal must already be preprocessed.
+
+Function: **compute_xb_Hxb**
+The **compute_xb_Hxb** function calculates discrete cosine set (xb) in microtime resulution and convolved discrete cosine set (Hxb) in scan resolution. Convolution is performed using canonical HRF (identical to SPM12 HRF). 
+
 
 ## Parameters
 **BOLD**(np.ndarray):
@@ -15,11 +19,10 @@ Preprocessed BOLD signal (numpy array).
 **alpha** (float, optional, default=0.005): Regularization parameter for ridge regression.
 
 **NT** (int, optional, default=16): Microtime resolution (number of time bins per scan).
-**xb** (np.ndarray, optional): Optional regressor matrix.
-**Hxb**  (np.ndarray, optional): Discrete kernel for computing regression.
+**xb** (np.ndarray, optional, default: discrete cosine set): Temporal basis set in microtime resolution.
+**Hxb**  (np.ndarray, optional, default: discrete cosine set convolved with canonical HRF): Convolved temporal basis set in scan resolution.
 
 **Returns** Neuronal time series (np.ndarray): The deconvolved neuronal time series.
-
 
 
 ## Basic usage
@@ -31,12 +34,12 @@ from bold_deconvolution import ridge_regress_deconvolution,  compute_xb_Hxb
 # Example usage
 preprocessed_BOLD = np.load('path_to_preprocessed_BOLD.npy')
 TR = 2.0
-alpha = 0.005
 NT = 16
+alpha = 0.005
 
 neuronal_time_series = ridge_regress_deconvolution(BOLD=preprocessed_BOLD, TR=TR, alpha=alpha, NT=NT)
 
-#precompute regressor matrix and kernel
+# Precompute cosine basis set (e.g., to apply the same temporal basis set for multiple BOLD-signal time series)
 xb, Hxb = compute_xb_Hxb(len(preprocessed_BOLD), NT, TR)
 
 neuro_ridge_precomp = ridge_regress_deconvolution(preprocessed_BOLD, TR, alpha, NT, xb=xb, Hxb=Hxb)
@@ -46,4 +49,4 @@ neuro_ridge_precomp = ridge_regress_deconvolution(preprocessed_BOLD, TR, alpha, 
 
 ## Notebook Example
 
-See usage example in [TestFunctions.ipynb](https://github.com/IHB-IBR-department/BoldDeconvolution/blob/main/TestFunctions.ipynb)
+See usage example in [usage_example.ipynb](https://github.com/IHB-IBR-department/BOLD_deconvolution/blob/main/usage_example.ipynb)
--- bold_deconvolution.py
+++ bold_deconvolution.py
@@ -1,53 +1,5 @@
 import numpy as np
-from numba import njit
 from scipy.stats import gamma
-from scipy import signal
-from scipy.fftpack import dct
-from nilearn.glm.first_level import spm_hrf, compute_regressor
-from sklearn.linear_model import Ridge
-
-
-def ridge_deconvolution(BOLD: np.ndarray,
-                        TR: float,
-                        alpha: float = 0.005,
-                        NT: int = 16,
-                        Hxb: np.ndarray = None) -> np.ndarray:
-    """
-    Deconvolves a preprocessed BOLD signal into neuronal time series
-    based on discrete cosine set and ridge regression.
-
-   :param BOLD: Preprocessed BOLD signal (numpy array)
-   :param TR: Time repetition, in seconds
-   :param alpha:  Regularization parameter, defaults to 0.005
-   :param NT: Microtime resolution (number of time bins per scan), defaults to 16
-   :param Hxb: discrete kernel for computing regression
-   :return: Deconvolved neuronal time series
-   """
-
-    N = len(BOLD)
-    # Create cosine basis set
-    xb = dctmtx_numpy_vect(NT * N + 128, N)
-
-    # Create Nilearn HRF with specified microtime resolution
-    time_length = 32
-    dt = TR / NT
-    frame_times = np.linspace(0, time_length, int(time_length / dt + 1))
-    onset, amplitude, duration = 0.0, 1.0, dt
-    exp_condition = np.array((onset, duration, amplitude)).reshape(3, 1)
-
-    nilearn_hrf = compute_regressor(exp_condition, "spm", frame_times, con_id="main")[0]
-
-    Hxb = np.zeros((N, N))
-
-    for i in range(N):
-        Hxb[:, i] = np.squeeze(signal.convolve(xb[:, i].reshape(-1, 1), nilearn_hrf)[1 + 128:N * NT + 128:NT])
-
-    reg_alpha = Ridge(alpha=alpha, solver='lsqr', fit_intercept=False, max_iter=1000)
-    reg_alpha.fit(Hxb, BOLD)
-
-    neuronal = np.matmul(xb[128:N * NT + 128, :], reg_alpha.coef_[0, :N])
-    return neuronal.flatten()
-
 
 def ridge_regress_deconvolution(BOLD: np.ndarray,
                                 TR: float,
@@ -57,22 +9,25 @@ def ridge_regress_deconvolution(BOLD: np.ndarray,
                                 Hxb: np.ndarray = None) -> np.ndarray:
     """
     Deconvolves a preprocessed BOLD signal into neuronal time series
-    based on discrete cosine set and ridge regression.
+    based on selected temporal basis set (by default: discrete cosine
+    set convolved with canonical HRF) and ridge regression.
 
     This function does not use confound regressors (e.g., motion).
     This function does not perform whitening and temporal filtering.
     The BOLD input signal must already be pre-processed.
 
     :param BOLD: Preprocessed BOLD signal (numpy array)
-    :type BOLD: numpy.ndarray
+    :type BOLD: numpy.ndarray (NxROI)
     :param TR: Time repetition, in seconds
     :type TR: float
     :param alpha: Regularization parameter, defaults to 0.005
     :type alpha: float, optional
     :param NT: Microtime resolution (number of time bins per scan), defaults to 16
     :type NT: int, optional
-    :param Hxb: discrete kernel for computing regression
-    :type BOLD: numpy.ndarray (NxN)
+    :param xb: Temporal basis set in microtime resolution
+    :type xb: numpy.ndarray (N*NTxN*NT)
+    :param Hxb: Convolved temporal basis set in scan resolution
+    :type Hxb: numpy.ndarray (NxN)
     :return: Deconvolved neuronal time series
     :rtype: numpy.ndarray
 
@@ -84,7 +39,7 @@ def ridge_regress_deconvolution(BOLD: np.ndarray,
         raise ValueError('BOLD signal is empty.')
     if TR == 0:
         raise ZeroDivisionError('TR should be more than 0')
-    dt = TR / NT  # Length of time bin, [s]
+    dt = TR / NT   # Length of time bin, [s]
     N = len(BOLD)  # Scan duration, [dynamics]
     k = np.arange(0, N * NT, NT)  # Microtime to scan time indices
 
@@ -94,8 +49,7 @@ def ridge_regress_deconvolution(BOLD: np.ndarray,
         raise ValueError("Both xb and Hxb should be specified, or neither of them")
 
     if xb is None:
-        xb, Hxb = compute_xb_Hxb(N, NT, TR)
-
+        xb, Hxb = compute_xb_Hxb(N, NT, TR) # Use discrete cosine set convolved with canonical HRF
 
     # Perform ridge regression
     C = np.linalg.solve(Hxb.T @ Hxb + alpha * np.eye(N), Hxb.T @ BOLD)
@@ -110,7 +64,7 @@ def compute_xb_Hxb(N: int, NT: int, TR: float) -> np.ndarray:
     dt = TR / NT
     k = np.arange(0, N * NT, NT)  # Microtime to scan time indices
 
-    # Create canonical HRF in microtime resolution (identical to SPM cHRF)
+    # Create canonical HRF in microtime resolution (identical to SPM12 cHRF)
     t = np.arange(0, 32 + dt, dt)
     hrf = gamma.pdf(t, 6) - gamma.pdf(t, NT) / 6
     hrf = hrf / np.sum(hrf)
@@ -126,29 +80,6 @@ def compute_xb_Hxb(N: int, NT: int, TR: float) -> np.ndarray:
     return xb, Hxb
 
 
-def dctmtx(N: int, K: int, is_numba=False) -> np.ndarray:
-    if is_numba:
-        C = dctmtx_numba(N, K)
-    else:
-        C = dctmtx_numba(N, K)
-    return C
-
-
-# Regular numpy version of dctmtx
-def dctmtx_numpy(N: int, K: int) -> np.ndarray:
-    n = np.arange(N)
-    C = np.zeros((N, K))
-    C[:, 0] = np.ones(N) / np.sqrt(N)
-    for k in range(1, K):
-        C[:, k] = np.sqrt(2 / N) * np.cos(np.pi * (2 * n) * k / (2 * N))
-    return C
-
-
-def dct_scipy():
-    # not implemented, but definitely need to check with dct
-    pass
-
-
 def dctmtx_numpy_vect(N: int, K: int) -> np.ndarray:
     n = np.arange(N)
     C = np.zeros((N, K))
@@ -158,12 +89,3 @@ def dctmtx_numpy_vect(N: int, K: int) -> np.ndarray:
     return C
 
 
-# Numba optimized version of dctmtx
-@njit(fastmath=True)
-def dctmtx_numba(N: int, K: int) -> np.ndarray:
-    n = np.arange(N)
-    C = np.zeros((N, K))
-    C[:, 0] = np.ones(N) / np.sqrt(N)
-    for k in range(1, K):
-        C[:, k] = np.sqrt(2 / N) * np.cos(np.pi * (2 * n) * k / (2 * N))
-    return C
